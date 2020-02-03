179a4cdd4aa0f0c98c3def9bbc9f0bae9b24c5e6
Tim Pietz <tim@pietz.me>
2020-02-03 13:24:11 +0100

    Use modules as entrypoint instead of selected functions

--- spiegel_scraper/__init__.py
+++ spiegel_scraper/__init__.py
@@ -1,3 +1,1 @@
-from spiegel_scraper.archive import by_date as archive_by_date
-from spiegel_scraper.article import by_url as article_by_url
-from spiegel_scraper.comments import by_article_id as comments_by_article_id
+from spiegel_scraper import archive, article, comments
