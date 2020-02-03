c4cd286dfa1c04a3e290ccf7cd1087478f4359cf
Tim Pietz <tim@pietz.me>
2020-02-03 13:19:12 +0100

    Rename talk module to comments

--- spiegel_scraper/__init__.py
+++ spiegel_scraper/__init__.py
@@ -1,3 +1,3 @@
 from spiegel_scraper.archive import by_date as archive_by_date
 from spiegel_scraper.article import by_url as article_by_url
-from spiegel_scraper.talk import by_article_id as talk_by_article_id
+from spiegel_scraper.comments import by_article_id as comments_by_article_id
